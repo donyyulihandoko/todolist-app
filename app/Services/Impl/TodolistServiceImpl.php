60f534dffe3bb29e2c6ea88c0079257a0ec0d510
Dony Yuli Handoko <officialdonyyulihandoko@gmail.com>
2025-12-18 22:33:07 +0700

    commit feature.todolist3

--- app/Services/Impl/TodolistServiceImpl.php
+++ app/Services/Impl/TodolistServiceImpl.php
@@ -4,6 +4,7 @@ namespace App\Services\Impl;
 
 use App\Models\Todolist;
 use App\Services\TodolistService;
+use Illuminate\Support\Facades\DB;
 
 class TodolistServiceImpl implements TodolistService
 {
@@ -25,5 +26,29 @@ class TodolistServiceImpl implements TodolistService
             'description' => $data['description'],
             'category_id' => $data['category_id']
         ];
+    }
+
+    public function getTodolistById(int $id)
+    {
+        return Todolist::query()->find($id);
+    }
+
+    public function updateTodolist(int $id, array $data)
+    {
+        $formTodo = $this->mapTodolistForm($data);
+        $todo = $this->getTodolistById($id);
+        if ($todo !== null) {
+            $todo->update($formTodo);
+        }
+    }
+
+    public function removeTodolist(int $id)
+    {
+        // $getTodolistById = $this->getTodolistById($id);
+        // if ($getTodolistById !== null) {
+        //     $getTodolistById->query()->delete();
+        // }
+        $todo = $this->getTodolistById($id);
+        if ($todo !== null) $todo->delete();
     }
 }
--- app/Services/TodolistService.php
+++ app/Services/TodolistService.php
@@ -7,4 +7,10 @@ interface TodolistService
     public function getTodolists();
 
     public function saveTodolist(array $data);
+
+    public function getTodolistById(int $id);
+
+    public function updateTodolist(int $id, array $data);
+
+    public function removeTodolist(int $id);
 }
